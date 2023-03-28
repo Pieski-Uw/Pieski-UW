5f93a0bd620ab514afb07d97ee9f9b79c3c442ef
Sulnek <franciszek.sulima@gmail.com>
2023-03-28 12:23:58 +0200

    Changes of types in dictionary. Changed "age" value in dictionary to int (number of months) and weight (number of kilograms). Also modified ifs in code to look prettier and fixed errors from Pylint

--- web_scraper.py
+++ web_scraper.py
@@ -2,64 +2,68 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
+
 def parse_pet(href):
-    html = requests.get(href)
+    """
+        Returns information about a pet, from a website "href" in
+        a dictionary. (value None if no information)
+
+        usage example: parse_pet('https://napaluchu.waw.pl/pet/012300408/')
+    """
+    html = __timed_get(href)
     html_text = html.text
     soup = BeautifulSoup(html_text, 'lxml')
-    details = soup.find('ul', class_ = 'petdetails')
+    details = soup.find('ul', class_='petdetails')
     indents = details.find_all('li')
-    #W poniższych zmiennych zapisze dane, None oznacza brak danych
-    age = None
-    breed = None
-    gender = None
-    weight = None
-    number = None
-    status = None
-    date_in = None
-    date_out = None
-    box = None
-    address_found = None
-    group_link = None
-    group_name = None
+    # W poniższym słowniku zapisze dane, None oznacza brak danych
+    info = {
+        "age": None,  # in months
+        "breed": None,
+        "gender": None,
+        "weight": None,  # in kilograms
+        "number": None,
+        "status": None,
+        "date_in": None,
+        "date_out": None,
+        "box": None,
+        "address_found": None,
+        "group_link": None,
+        "group_name": None,
+    }
+
     for indent in indents:
-        if (indent.text.startswith("W typie rasy")):
-            breed = indent.find('strong').text
-        elif (indent.text.startswith("Wiek")):
-            age = indent.find('strong').text
-        elif (indent.text.startswith("Płeć")):
-            gender = indent.find('strong').text
-        elif (indent.text.startswith("Waga")):
-            weight = indent.find('strong').text
-        elif (indent.text.startswith("Numer")):
-            number = indent.find('strong').text
-        elif (indent.text.startswith("Status")):
-            status = indent.find('strong').text
-        elif (indent.text.startswith("Przyjęty")):
-            date_in = indent.find('strong').text
-        elif (indent.text.startswith("Wydany")):
-            date_out = indent.find('strong').text
-        elif (indent.text.startswith("Znaleziony")):
-            address_found = indent.find('strong').text
-        elif (indent.text.startswith("Boks")):
-            box = indent.find('strong').text
-        elif (indent.text.startswith("Grupa")):
-            group_name = indent.find('strong').text
-            group_link = indent.find('a').get('href')
-    #to be removed but may be useful in debug:
-#     print(f'''
-# Breed: {breed}
-# Age: {age}
-# Gender: {gender}
-# Weight: {weight}
-# Number: {number}
-# Status: {status}
-# date_in: {date_in}
-# date_out: {date_out}
-# address_found: {address_found}
-# box: {box}
-# group_name: {group_name}
-# group_link: {group_link}
-# ''')
+        if not indent.find('strong'):  # site layout changed significantly
+            pass
+        if indent.text.startswith("W typie rasy"):
+            info["breed"] = indent.find('strong').text
+        elif indent.text.startswith("Wiek"):
+            if indent.find('strong'):
+                age_string = indent.find('strong').text
+                num, unit = age_string.split()
+                if unit.startswith("l"):
+                    info["age"] = 12 * int(num)
+                else:  # unit.startswith("m") - months
+                    info["age"] = int(num)
+        elif indent.text.startswith("Płeć"):
+            info["gender"] = indent.find('strong').text
+        elif indent.text.startswith("Waga"):
+            info["weight"] = int(indent.find('strong').text.split()[0])
+        elif indent.text.startswith("Nr"):
+            info["number"] = indent.find('strong').text
+        elif indent.text.startswith("Status"):
+            info["status"] = indent.find('strong').text
+        elif indent.text.startswith("Przyjęty"):
+            info["date_in"] = indent.find('strong').text
+        elif indent.text.startswith("Wydany"):
+            info["date_out"] = indent.find('strong').text
+        elif indent.text.startswith("Znaleziony"):
+            info["address_found"] = indent.find('strong').text
+        elif indent.text.startswith("Boks"):
+            info["box"] = indent.find('strong').text
+        elif indent.text.startswith("Grupa"):
+            info["group_name"] = indent.find('strong').text
+            info["group_link"] = indent.find('a').get('href') if indent.find('a') else None
+    return info
 
 def __timed_get(href):
     """Performs get request with preset timeout"""
@@ -78,7 +82,7 @@ def get_links_to_animals_from_page(href: str) -> list[str]:
     html_links = soup.find_all('a', href=re.compile('/pet/'), text='dowiedz się więcej')
 
     result: list[str] = list(map(lambda tag: tag.get('href'), html_links))
-    
+
     return result
 
 def get_links_to_all_animal(href: str) -> set[str]:
@@ -102,6 +106,6 @@ def get_links_to_all_animal(href: str) -> set[str]:
 
     return result
 
-#parse_pet('https://napaluchu.waw.pl/pet/012300408/')
-#get_links_to_animals_from_page('https://napaluchu.waw.pl/zwierzeta/znalazly-dom/?pet_page=1')
-#get_links_to_all_animal('https://napaluchu.waw.pl/zwierzeta/znalazly-dom')
+# print(parse_pet('https://napaluchu.waw.pl/pet/012300408/'))
+# get_links_to_animals_from_page('https://napaluchu.waw.pl/zwierzeta/znalazly-dom/?pet_page=1')
+# get_links_to_all_animal('https://napaluchu.waw.pl/zwierzeta/znalazly-dom')
