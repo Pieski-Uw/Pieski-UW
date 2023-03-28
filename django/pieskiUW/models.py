855b8ed807ad3103432ca25674006d052931c092
Justyna Palikowska <jp439983@orange03.mimuw.edu.pl>
2023-03-28 10:53:00 +0200

    Finished models.py

--- django/pieskiUW/models.py
+++ django/pieskiUW/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-class Links(models.Model):
-    link = models.URLField()
-
 class Dog(models.Model):
-    AGE_UNIT = (
-        ('m', 'month(s)'),
-        ('y', 'year(s)'),
-    )
     SEX = (
         ('m', 'male'),
         ('f', 'female'),
@@ -16,13 +9,13 @@ class Dog(models.Model):
     name = models.CharField(max_length=20)
     breed = models.CharField(max_length=40)
     age = models.IntegerField()
-    age_unit = models.CharField(max_length=1, choices=AGE_UNIT)
     gender = models.CharField(max_length=1, choices=SEX)
     weight = models.IntegerField()
-    accepted = models.DateField()
-    released = models.DateField()
-    found_city = models.CharField(max_length=20)
-    found_street = models.CharField(max_length=40)
-    boxing = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    date_in = models.DateField()
+    date_out = models.DateField()
+    address_found = models.CharField(max_length=50)
+    box = models.CharField(max_length=20)
     group_name = models.CharField(max_length=20)
     group_link = models.URLField()
+    link = models.URLField()
