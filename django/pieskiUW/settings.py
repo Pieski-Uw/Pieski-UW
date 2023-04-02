2ac4f4043a479e426a63d74fa971d4d7af23c66d
Artur Kamieniecki <kamart02@wp.pl>
2023-04-02 21:02:39 +0200

    Fixed pylint error

--- django/pieskiUW/settings.py
+++ django/pieskiUW/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
+# pylint: disable=no-name-in-module
+
 from .secrets import postgresql
+
+# pylint: enable=no-name-in-module
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
