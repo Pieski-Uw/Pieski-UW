2ce5ba167c6033787322f43bce4192655b7ca386
Sulnek <Franio@DESKTOP-TQ262LS>
2023-05-16 20:32:45 +0200

    Fixing problems detected in the last review

--- django/pieskiUW/settings.py
+++ django/pieskiUW/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-=4941=zsj*p&hnlm24dm#p1ihb%6w!!p8j**7bf7$ae4=zxopo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 ALLOWED_HOSTS += postgresql.SECRETS["HOST"]
 
 
