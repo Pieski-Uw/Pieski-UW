98dc24b820f70612687cd29ef420b5ff8b8f06cc
Mateusz Wasilewski Ubuntu <mateuszwasilewski12@gmail.com>
2023-06-07 13:18:57 +0200

    Fix unit tests after using celery

---
+++ django/tests/test_data/animal/milka.py
@@ -0,0 +1,17 @@
+DATA = {
+    "name": "Milka",
+    "number": "2005/21",
+    "breed": "Mieszaniec",
+    "age": 7 * 12,
+    "weight": 23,
+    "gender": "samica",
+    "status": "wydane",
+    "address_found": "Powrót z DT",
+    "box": "332 R11 (domek)",
+    "group_name": "Żółte Boksy",
+    "group_link": "https://www.facebook.com/grupabehawioralna/",
+}
+
+LINK = "https://napaluchu.waw.pl/pet/012103435/"
+
+MOCK_FILE = "django/tests/test_data/animal/milka.html"
