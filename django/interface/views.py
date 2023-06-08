eb1cb7342142e09126f943bb6625d31e344dc8dc
jpalikowska <115477216+jpalikowska@users.noreply.github.com>
2023-06-08 22:36:49 +0000

    not finished, not good looking table with links and displaying only for weight 50 or age 6

--- django/interface/views.py
+++ django/interface/views.py
@@ -3,6 +3,7 @@ from webscraper.models import Pet
 from django.db.models import Count
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.core import serializers
 
 
 def index(request):
@@ -30,3 +31,13 @@ def count_pets_by_weight(request):
     """API view that counts all pets that have the same weight and returns a JSON object"""
     pets_by_weight = Pet.objects.values("weight").annotate(count=Count("pk"))
     return JsonResponse({"pets_by_weight": list(pets_by_weight)})
+
+def get_pets_by_weight(request,id):
+    """API view that returns in JSON all pets that have weight = id"""
+    petss = serializers.serialize("json", Pet.objects.filter(weight = id))
+    return JsonResponse({"pets": petss})
+
+def get_pets_by_age(request,id):
+    """API view that returns in JSON all pets that have age = id"""
+    petss = serializers.serialize("json", Pet.objects.filter(age = id))
+    return JsonResponse({"pets": petss})
